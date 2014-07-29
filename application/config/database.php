0f4d79904c45163528b7475f9e7632ef04e73cb5
szqsyt0 <szqsyt0@gmail.com>
2014-07-29 23:06:59 +0800

    添加了一个美(lan)如(da)画(jie)的后台界面

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'itmvuser';
-$db['default']['password'] = 'texk$u123';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'itmv';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
